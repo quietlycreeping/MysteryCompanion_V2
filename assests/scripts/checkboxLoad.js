a3fd4139df7f029f587ce001a334dafaec6e1db7
quietlycreeping <60121681+quietlycreeping@users.noreply.github.com>
2025-09-17 20:49:00 -0400

    load in

---
+++ assests/scripts/checkboxLoad.js
@@ -0,0 +1,19 @@
+//array of all classes of checkboxes
+var boxes = Array.from(document.querySelectorAll('[id^="objective"]'));
+
+//array of all checkbox ids
+var boxID = [];
+for (let x=0; x < boxes.length; x++)
+    {
+        let id = boxes[x].id; 
+        boxID.push(id);
+    }
+
+//load checkbox state from Local Storage
+let checkedStringStorage = localStorage.getItem("checkboxes");
+let isCheckedStorage = JSON.parse(checkedStringStorage);
+
+for (let x=0; x < isCheckedStorage.length; x++)
+    {
+        document.getElementById(boxID[x]).checked = isCheckedStorage[x];
+    }        
