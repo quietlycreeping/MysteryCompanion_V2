c6549a2cd5b3aa1bd30335967fe99526f28528c0
quietlycreeping <60121681+quietlycreeping@users.noreply.github.com>
2025-09-21 12:55:36 -0400

    Orginization

--- assests/scripts/countdown.js
+++ assests/scripts/countdown.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 14, 2024 16:00:00").getTime();
+var countDownDate = new Date("Oct 18, 2025 16:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() 
