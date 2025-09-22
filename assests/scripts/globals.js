845b83ecf6e70f31ca7ac420c456dcbec2f65fdb
quietlycreeping <60121681+quietlycreeping@users.noreply.github.com>
2025-09-22 16:09:40 -0400

    about

---
+++ assests/scripts/globals.js
@@ -0,0 +1,8 @@
+//Did the party start? Hide tasks until party.
+const  party = false;
+
+//did the murder occur? In second half of party? Hide second tasks and edvindence.
+const  murder = false;
+
+//Are they logged in? And who? Null if not logged in. Help lead to correct profile page.
+var loggedIn = "null";
